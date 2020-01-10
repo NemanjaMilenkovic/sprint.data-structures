6b771e2dc23fb015c753e10af52fbe5a644383c8
nemanjamilenkovic <nemanja.milenkovic@gmail.com>
2020-01-10 14:02:14 +0900

    started function traverseTree

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,4 +1,47 @@
-class BinarySearchTree {}
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = [];
+    this.right = [];
+  }
+  insert(toInsert) {
+    let direction = true;
+
+    function traverseTree(node) {
+      // 1. we need to get to an end of the tree (recurse untill both .left and .right are empty [])
+      // 2. once we get to the end, we need to check if value is supposed to go to the .left or .right
+      // 3. we need to create new node and assign toInsert to the node value
+      // console.log("test1", Array.isArray(node.left) && node.left.length);
+
+      // node.left[0].value
+
+      if (!(node.left[0] && node.right[0])) {
+        if (toInsert < node.value) {
+          node.left.push(new BinarySearchTree(toInsert));
+          return;
+        }
+
+        if (toInsert > node.value) {
+          node.right.push(new BinarySearchTree(toInsert));
+          return;
+        }
+      } else {
+        if (node.value > toInsert) {
+          traverseTree(node.left[0]);
+        }
+
+        if (node.value < toInsert) {
+          traverseTree(node.right[0]);
+        }
+      }
+    }
+    let l = this;
+    traverseTree(l);
+    return this;
+  }
+}
+
+module.exports = BinarySearchTree;
 
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
--- src/Tree.js
+++ src/Tree.js
@@ -4,7 +4,47 @@ class Tree {
     this.children = [];
   }
 
-  addChild(value) {}
+  addChild(value) {
+    function recurse(node) {
+      // let direction;
+
+      // if (direction !== undefined && direction.length === 0) {
+      //   direction.push(new Tree(value));
+      // }
+
+      //  if (node.left.length === 0 && node.right.len) {
+      //   node.children[0].push(new Tree(value));
+      // }
+      console.log("nodeChildren", node.children !== undefined);
+
+      console.log("ssa");
+      if (
+        node.children[0] !== undefined > 0 &&
+        node.children[0].value !== undefined &&
+        node.children[0].value > value
+      ) {
+        console.log(node.children[0]);
+        recurse(node.children[0]);
+
+        // node.left.value = value;
+        // node.
+      }
+
+      if (
+        node.children[1] !== undefined &&
+        node.children[1].value !== undefined &&
+        node.children[1].value < value
+      ) {
+        console.log(node.children[0]);
+        recurse(node.children[1]);
+
+        // node.left.value = value;
+        // node.
+      }
+    }
+    const node = this;
+    recurse(node);
+  }
 
   contains(value) {}
 
@@ -22,6 +62,8 @@ requirements for ALL data structures in this exercise.
 
   traverseBreadthFirst(fn) {}
 }
+
+module.exports = Tree;
 
 /*
 |XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
