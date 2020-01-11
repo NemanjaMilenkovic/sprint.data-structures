9e6cd4e024e0c47450336e0e4a5abde2586ea168
nemanjamilenkovic <nemanja.milenkovic@gmail.com>
2020-01-11 16:08:29 +0900

    finished insert() recursive

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -4,69 +4,31 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
-  insert(toInsert) {
-    const topNode = this;
 
-    function findNode(toInsert) {
-      let node = topNode;
-      // console.log(node.left);
-      while (node.left !== null && node.right !== null) {
-        if (node.left != null) {
-          node = node.left;
+  insert(valueToInsert) {
+    const newNodeTree = new BinarySearchTree(valueToInsert);
+    function traverseTree(node) {
+      if (!node) return newNodeTree;
+      if (valueToInsert == node.value) return;
+
+      if (valueToInsert < node.value) {
+        if (!node.left) {
+          node.left = newNodeTree;
+          return;
+        } else {
+          traverseTree(node.left);
         }
-        if (node.right != null) {
-          node = node.right;
+      } else {
+        if (!node.right) {
+          node.right = newNodeTree;
+          return;
+        } else {
+          traverseTree(node.right);
         }
-      }
-      if (toInsert < node.value) {
-        node.left = new BinarySearchTree(toInsert);
-      } else if (toInsert > node.value) {
-        node.right = new BinarySearchTree(toInsert);
       }
     }
-    findNode(toInsert);
-    console.log(this);
+    traverseTree(this);
     return this;
-
-    // function traverseTree(node) {
-    //   // 1. does our node have children?
-    //   // 2. are they both empty?
-    //   // 3 if so, new tree --> check if insert is lower or higher than node.value
-    //   // 4. store the null node for later eg --> if insert value is lower than top node
-    //   // 5.
-
-    //   console.log("the first node is", topNode);
-
-    //   if (node && node.left === null && node.right === null) {
-    //     if (toInsert === node.value) {
-    //       return;
-    //     }
-    //     if (toInsert < node.value) {
-    //       node.left = new BinarySearchTree(toInsert);
-    //       return;
-    //     } else if (toInsert > node.value) {
-    //       node.right = new BinarySearchTree(toInsert);
-    //       return;
-    //     }
-    //   }
-
-    //   if (toInsert < topNode.value) {
-    //     console.log("value is", node.value);
-    //   }
-
-    //   if (node && node.value > toInsert && topNode.value > toInsert) {
-    //     console.log("node value", node.value);
-    //     traverseTree(node.left);
-    //   } else if (node && node.value < toInsert && topNode.value > toInsert) {
-    //     console.log(node.right);
-    //     traverseTree(node.right);
-    //   }
-    //   // else if (node && toInsert < topNode) {}
-    // }
-
-    // traverseTree(this);
-    // // console.log(this);
-    // // return this;
   }
 }
 
